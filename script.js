2036fa3c38d9e501f1b40513766238dc8a4f227a
Daniya.Kuspanova <Daniya.Kuspanova@nexign.com>
2025-03-07 23:47:14 +0400

    поправила все ошибки

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calculate() {
         const resultsBox = document.getElementById("results");
 
         if (isNaN(num1) || isNaN(num2)) {
-            throw new Error("Ошибка! Введите корректные числа.");
+            throw new Error("Ошибка: числа некорректные.");
         }
 
         let result;
@@ -22,33 +22,25 @@ function calculate() {
                 break;
             case "/":
                 if (num2 === 0) {
-                    throw new Error("Ошибка: Деление на ноль!");
+                    throw new Error("Ошибка: деление на ноль запрещено.");
                 }
                 result = (num1 / num2).toFixed(2);
                 break;
             default:
                 throw new Error("Неизвестная операция.");
         }
-
-        addResult(resultsBox, `${num1} ${operator} ${num2} = ${result}`);
-
+        const previousResults = resultsBox.getElementsByClassName("result-item");
+        for (let item of previousResults) {
+            item.classList.add("old-result");
+        }
+        const newResult = document.createElement("div");
+        newResult.classList.add("result-item");
+        newResult.innerHTML = "<b>" + num1 + operator + num2 + "=" + result + "</b>";
+        resultsBox.append(newResult);
         if (resultsBox.children.length > 5) {
             resultsBox.removeChild(resultsBox.firstChild);
         }
     } catch (error) {
         alert(error.message);
     }
-}
-
-function addResult(resultsBox, resultText) {
-    const previousResults = resultsBox.getElementsByClassName("result-item");
-    for (let i = 0; i < previousResults.length; i++) {
-        previousResults[i].classList.add("old-result");
-    }
-
-    const newResult = document.createElement("div");
-    newResult.classList.add("result-item");
-    newResult.innerHTML = `<b>${resultText}</b>`;
-
-    resultsBox.append(newResult);
 }
